d2116241737044e63e9c75666dd96c1e52fc3ec0
VKarpovV <valerok1980@gmail.com>
2025-07-07 14:08:44 +0300

    Update vm2_setup.sh

--- scripts/vm2_setup.sh
+++ scripts/vm2_setup.sh
@@ -14,22 +14,26 @@ sudo apt-get install -y apache2
 sudo systemctl enable apache2
 
 # Configure Apache
-sudo cp /home/kva/otus25/configs/apache/apache2.conf /etc/apache2/apache2.conf
-sudo cp /home/kva/otus25/configs/apache/sites-available/* /etc/apache2/sites-available/
+sudo mkdir -p /etc/apache2/sites-available
+sudo cp /home/kva/otus25/configs/apache/apache2.conf /etc/apache2/
+sudo cp /home/kva/otus25/configs/apache/default /etc/apache2/sites-available/
 sudo a2ensite default
 sudo a2enmod rewrite
 
 # Install MySQL Master
-/home/kva/otus25/scripts/mysql_master_setup.sh
+bash /home/kva/otus25/scripts/mysql_master_setup.sh
 
 # Install FileBeat
 sudo dpkg -i /home/kva/elk-8.9-deb/filebeat-8.9.1-amd64.deb
-sudo cp /home/kva/otus25/configs/elk/filebeat.yml /etc/filebeat/filebeat.yml
+sudo mkdir -p /etc/filebeat
+sudo cp /home/kva/otus25/configs/elk/filebeat/filebeat.yml /etc/filebeat/
+sudo chown root:root /etc/filebeat/filebeat.yml
+sudo chmod 640 /etc/filebeat/filebeat.yml
 sudo systemctl enable filebeat
 sudo systemctl start filebeat
 
 # Install ELK stack
-/home/kva/otus25/scripts/elk_setup.sh
+bash /home/kva/otus25/scripts/elk_setup.sh
 
 # Restart Apache
 sudo systemctl restart apache2
