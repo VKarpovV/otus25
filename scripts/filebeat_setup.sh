50c38c32feabbe1562ff17ca2ad35feb87b391ab
VKarpovV <valerok1980@gmail.com>
2025-07-07 13:57:52 +0300

    Update filebeat_setup.sh

--- scripts/filebeat_setup.sh
+++ scripts/filebeat_setup.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
-# Install Filebeat
+# Установка Filebeat
 sudo dpkg -i /home/kva/elk-8.9-deb/filebeat-8.9.1-amd64.deb
 
-# Configure Filebeat
-sudo cp /home/kva/otus25/configs/elk/filebeat.yml /etc/filebeat/filebeat.yml
+# Копирование конфига в зависимости от типа сервера
+if [ -f "/etc/nginx/nginx.conf" ]; then
+  # Это NGINX сервер
+  sudo cp /home/kva/otus25/configs/elk/filebeat/nginx-filebeat.yml /etc/filebeat/filebeat.yml
+elif [ -f "/etc/apache2/apache2.conf" ]; then
+  # Это Apache сервер
+  sudo cp /home/kva/otus25/configs/elk/filebeat/apache-filebeat.yml /etc/filebeat/filebeat.yml
+else
+  # Базовый конфиг
+  sudo cp /home/kva/otus25/configs/elk/filebeat/filebeat.yml /etc/filebeat/filebeat.yml
+fi
+
+sudo chown root:filebeat /etc/filebeat/filebeat.yml
+sudo chmod 640 /etc/filebeat/filebeat.yml
+
+# Настройка модулей
 sudo filebeat modules enable system
 sudo filebeat setup
+
+# Запуск Filebeat
 sudo systemctl enable filebeat
 sudo systemctl start filebeat
 
-echo "Filebeat setup completed!"
+echo "Filebeat установлен и настроен!"
