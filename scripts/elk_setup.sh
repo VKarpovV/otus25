447e99cbf668328de7f3fb444ee6f2ce018f1733
VKarpovV <valerok1980@gmail.com>
2025-07-07 13:57:25 +0300

    Update elk_setup.sh

--- scripts/elk_setup.sh
+++ scripts/elk_setup.sh
@@ -1,21 +1,22 @@
-#!/bin/bash
-
-# Install Elasticsearch
+# Установка Elasticsearch
 sudo dpkg -i /home/kva/elk-8.9-deb/elasticsearch-8.9.1-amd64.deb
-sudo cp /home/kva/otus25/configs/elk/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+sudo cp /home/kva/otus25/configs/elk/elasticsearch/elasticsearch.yml /etc/elasticsearch/
+sudo chown root:elasticsearch /etc/elasticsearch/elasticsearch.yml
 sudo systemctl enable elasticsearch
 sudo systemctl start elasticsearch
 
-# Install Logstash
+# Установка Logstash
 sudo dpkg -i /home/kva/elk-8.9-deb/logstash-8.9.1-amd64.deb
-sudo cp /home/kva/otus25/configs/elk/logstash.conf /etc/logstash/conf.d/
+sudo cp /home/kva/otus25/configs/elk/logstash/logstash.conf /etc/logstash/conf.d/
+sudo chown root:logstash /etc/logstash/conf.d/logstash.conf
 sudo systemctl enable logstash
 sudo systemctl start logstash
 
-# Install Kibana
+# Установка Kibana
 sudo dpkg -i /home/kva/elk-8.9-deb/kibana-8.9.1-amd64.deb
-sudo cp /home/kva/otus25/configs/elk/kibana.yml /etc/kibana/
+sudo cp /home/kva/otus25/configs/elk/kibana/kibana.yml /etc/kibana/
+sudo chown root:kibana /etc/kibana/kibana.yml
 sudo systemctl enable kibana
 sudo systemctl start kibana
 
-echo "ELK stack setup completed!"
+echo "ELK stack установлен и настроен!"
