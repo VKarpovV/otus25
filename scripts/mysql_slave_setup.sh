d7762c39b4af62b7e135a241886034f4e6cc87c2
VKarpovV <valerok1980@gmail.com>
2025-07-07 13:30:38 +0300

    Update mysql_slave_setup.sh

--- scripts/mysql_slave_setup.sh
+++ scripts/mysql_slave_setup.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
-# Запуск MySQL Slave в Docker
-docker-compose -f docker-compose/mysql-compose.yml up -d mysql_slave
+# Install MySQL 8
+sudo apt-get install -y mysql-server
 
-# Ожидание запуска MySQL
-sleep 30
+# Configure MySQL Slave
+sudo cp /home/kva/otus25/configs/mysql/slave.cnf /etc/mysql/mysql.conf.d/replication.cnf
+sudo systemctl restart mysql
 
-# Получение позиции репликации с мастера
-MASTER_STATUS=$(ssh kva@192.168.140.132 "docker exec mysql_master mysql -uroot -prootpassword -e 'SHOW MASTER STATUS\G'")
+# Configure replication
+MASTER_LOG_FILE=$(grep "File" /home/kva/mysql_master_status.txt | awk '{print $2}')
+MASTER_LOG_POS=$(grep "Position" /home/kva/mysql_master_status.txt | awk '{print $2}')
 
-MASTER_LOG_FILE=$(echo "$MASTER_STATUS" | awk '/File:/ {print $2}')
-MASTER_LOG_POS=$(echo "$MASTER_STATUS" | awk '/Position:/ {print $2}')
+sudo mysql -e "CHANGE MASTER TO MASTER_HOST='192.168.140.133', MASTER_USER='replicator', MASTER_PASSWORD='replicator_password', MASTER_LOG_FILE='$MASTER_LOG_FILE', MASTER_LOG_POS=$MASTER_LOG_POS;"
+sudo mysql -e "START SLAVE;"
 
-# Настройка репликации на славе
-docker exec mysql_slave mysql -uroot -prootpassword -e "
-CHANGE MASTER TO
-MASTER_HOST='192.168.140.132',
-MASTER_USER='replica',
-MASTER_PASSWORD='replica_password',
-MASTER_LOG_FILE='$MASTER_LOG_FILE',
-MASTER_LOG_POS=$MASTER_LOG_POS;
-START SLAVE;
-"
-
-echo "MySQL Slave настроен"
+echo "MySQL Slave setup completed!"
