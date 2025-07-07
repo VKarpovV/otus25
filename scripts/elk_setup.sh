924a766ce5bb22ed49b07cdff4250ebe36c76120
VKarpovV <valerok1980@gmail.com>
2025-07-07 14:16:39 +0300

    Update elk_setup.sh

--- scripts/elk_setup.sh
+++ scripts/elk_setup.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 # Установка Elasticsearch
 sudo dpkg -i /home/kva/elk-8.9-deb/elasticsearch-8.9.1-amd64.deb
+sudo mkdir -p /etc/elasticsearch
 sudo cp /home/kva/otus25/configs/elk/elasticsearch/elasticsearch.yml /etc/elasticsearch/
 sudo chown root:elasticsearch /etc/elasticsearch/elasticsearch.yml
 sudo systemctl enable elasticsearch
@@ -7,6 +10,7 @@ sudo systemctl start elasticsearch
 
 # Установка Logstash
 sudo dpkg -i /home/kva/elk-8.9-deb/logstash-8.9.1-amd64.deb
+sudo mkdir -p /etc/logstash/conf.d
 sudo cp /home/kva/otus25/configs/elk/logstash/logstash.conf /etc/logstash/conf.d/
 sudo chown root:logstash /etc/logstash/conf.d/logstash.conf
 sudo systemctl enable logstash
@@ -14,6 +18,7 @@ sudo systemctl start logstash
 
 # Установка Kibana
 sudo dpkg -i /home/kva/elk-8.9-deb/kibana-8.9.1-amd64.deb
+sudo mkdir -p /etc/kibana
 sudo cp /home/kva/otus25/configs/elk/kibana/kibana.yml /etc/kibana/
 sudo chown root:kibana /etc/kibana/kibana.yml
 sudo systemctl enable kibana
